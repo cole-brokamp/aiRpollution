adcd312c4ca2ffee8fa2cfe39a090491f30745c0
Cole Brokamp <cole.brokamp@gmail.com>
2015-09-09 15:28:53 -0400

    added documentation to predictPollution

--- R/example.R
+++
@@ -1,7 +0,0 @@
-## example
-
-sample.loc <- data.frame('x'=-84.5371597,'y'=39.1603015)
-coordinates(sample.loc) <- c('x','y')
-sp::proj4string(sample.loc) <- CRS("+init=epsg:4326")
-
-XX <- predictPollution(loc=sample.loc,element='TRAP',model.type='rf')
--- R/predictPollution.R
+++ R/predictPollution.R
@@ -1,3 +1,16 @@
+#' predict pollution exposure
+#'
+#' This function is an implementation of the elemental PM exposure (random forest and regression) models developed for the Cincinnati Children's Asthma and Air Pollution Study (CCAAPS).  The underlying functions are not available to the user.  Not meant to be a generalizable package, its sole purpose is to generate exposure estimates for the Cincinnati area.
+#' @param loc the location for which to estimate the concentration (must be a spatial object and have a valid proj4string)
+#' @param element element for which to predict the concentration (one of "Cu", "Fe", "Zn", "S", "Ni", "V", "Si", "K", "Pb", "Mn", "Al", "TRAP", "PM25")
+#' @param model.type (either "rf" for random forest or "lm" for regression)
+#' @export
+#' @examples
+#' sample.loc <- data.frame('x'=-84.5371597,'y'=39.1603015)
+#' coordinates(sample.loc) <- c('x','y')
+#' sp::proj4string(sample.loc) <- CRS("+init=epsg:4326")
+#' predictPollution(loc=sample.loc,element='TRAP',model.type='rf')
+
 predictPollution <- function(loc,element,model.type) {
   stopifnot(model.type %in% c('rf','lm'))
   stopifnot(element %in% names(LU.final.lm.models))

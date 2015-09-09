36b31bce9a06a6ed742489f38f51d71c6fd22806
Cole Brokamp <cole.brokamp@gmail.com>
2015-09-09 15:18:23 -0400

    added sysdata.rda

--- R/example.R
+++ R/example.R
@@ -1,5 +1,7 @@
 ## example
+
 sample.loc <- data.frame('x'=-84.5371597,'y'=39.1603015)
 coordinates(sample.loc) <- c('x','y')
 sp::proj4string(sample.loc) <- CRS("+init=epsg:4326")
+
 XX <- predictPollution(loc=sample.loc,element='TRAP',model.type='rf')

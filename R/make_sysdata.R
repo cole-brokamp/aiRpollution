32547015da5bdb581a92925d46b4d3c45f4dd533
Cole Brokamp <cole.brokamp@gmail.com>
2015-09-11 14:49:24 -0400

    script to make sysdata.rda

---
+++ R/make_sysdata.R
@@ -0,0 +1,17 @@
+# # load in final models
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/final land use LM models.RData')
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/final land use RF models.RData')
+#
+# # load in source GIS files
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_NEI.RData')
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_census.RData')
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_greenspace.RData')
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_landcoverdb.RData')
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_old_ECAT.RData')
+# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/TIGER_2002.RData')
+#
+# devtools::use_data(bus_lines,d.intersections,d.NEI,deprivation.shapes,elevation.raster,
+#                    greenspace.raster,highway.traffic,interstate.traffic,landcover.raster,
+#                    LU.final.lm.models,LU.final.rf.models,population.shapes,railroads,
+#                    roads_1,roads_2,roads_3,roads_4,roads_5,
+#                    internal=TRUE,overwrite=TRUE,compress='gzip')

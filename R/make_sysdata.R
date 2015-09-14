b19585253a62a5304420332e35dee3cae80d87e3
Cole Brokamp <cole.brokamp@gmail.com>
2015-09-14 13:15:43 -0400

    fixed references to raster files in sysdata.rda

--- R/make_sysdata.R
+++ R/make_sysdata.R
@@ -5,11 +5,20 @@
 # # load in source GIS files
 # load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_NEI.RData')
 # load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_census.RData')
-# load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_greenspace.RData')
 # load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_landcoverdb.RData')
 # load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/LU_data_old_ECAT.RData')
 # load('/Users/cole/Documents/Biostatistics/Elemental Land Use Models/final_models/TIGER_2002.RData')
 #
+# elevation.raster <- readAll(elevation.raster)
+# landcover.raster <- readAll(landcover.raster)
+#
+#
+# greenspace.raster <- raster('/Users/cole/Documents/Biostatistics/_GIS data/Greenspace NDVI rasters/ndvi2000_3735.tif')
+# proj4string(greenspace.raster) <- CRS('+init=epsg:3735')
+# greenspace.raster <- readAll(greenspace.raster)
+#
+#
+# #
 # devtools::use_data(bus_lines,d.intersections,d.NEI,deprivation.shapes,elevation.raster,
 #                    greenspace.raster,highway.traffic,interstate.traffic,landcover.raster,
 #                    LU.final.lm.models,LU.final.rf.models,population.shapes,railroads,
